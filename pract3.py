1b71685755cd1a9927b2a516f10b4d8548bb0d2e
manighahrmani <mashapicasso@gmail.com>
2024-10-02 14:23:12 +0100

    Update based on graphix

--- pract3.py
+++ pract3.py
@@ -1,28 +1,28 @@
-from graphics import *
+from graphix import Window, Text, Circle, Line, Point
 
 
-def helloGraphics():
-    win = GraphWin()
+def hello_graphics():
+    win = Window()
     message = Text(Point(100, 100), "Hello graphics!")
     message.draw(win)
-    win.getMouse()
+    win.get_mouse()
 
 
-def drawStickFigure():
-    win = GraphWin("Stick figure")
+def draw_stick_figure():
+    win = Window("Stick figure")
     head = Circle(Point(100, 60), 20)
     head.draw(win)
     body = Line(Point(100, 80), Point(100, 120))
     body.draw(win)
 
 
-def drawLine():
-    win = GraphWin("Line drawer")
+def draw_line():
+    win = Window("Line drawer")
     message = Text(Point(100, 100), "Click on first point")
     message.draw(win)
-    p1 = win.getMouse()
-    message.setText("Click on second point")
-    p2 = win.getMouse()
+    p1 = win.get_mouse()
+    message.text = "Click on second point"
+    p2 = win.get_mouse()
     line = Line(p1, p2)
     line.draw(win)
-    message.setText("")
+    message.text = ""
